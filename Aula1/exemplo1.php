ab0487c983b1de163bf885c7ce7f210c0f16fa54
Maily <maily.ramos@aluno.senai.br>
2025-10-16 08:30:20 -0300

    php
    php basico

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+    //Geração de texto (string)
+    echo "Chama Sesi-Senai!";
+
+    /*
+    Geração de texto estraturado
+    Com tags e atributos
+    */
+    // Aspas simples dentro das aspas duplas
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+
+    //Aspas dupla dentro de aspas dupla
+    echo "<h2>Gerando <span class=\"destaque\"TML</span> através do PHP.</h2";
+?>
